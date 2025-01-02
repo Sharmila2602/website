f653bc7aed83e031d0bb519c0c252abb5098327e
Sharmila2602 <114284547+Sharmila2602@users.noreply.github.com>
2025-01-02 15:06:38 +0530

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,7 @@
+function sun(){
+    var a=document.getElementById('aa');
+    document.write('Your Response was saved.'+"<br>"+"<a href='web.htm'>Go To HomePage.</a>");
+    document.body.style.color="green";
+    document.body.style.fontSize="30px";
+    document.body.style.textAlign="center";
+}
